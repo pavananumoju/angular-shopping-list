becde1fa97b3f34902b7acdc6a2e8a1612798202
pavananumoju <pavananumoju@gmail.com>
2021-09-16 19:49:30 +0530

    reactive forms: Recipe module

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,6 +5,7 @@ import { Recipe } from "./recipe.model";
 
 export class RecipeService{
     recipeSelected = new Subject<Recipe>();
+    recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
         new Recipe(
@@ -33,5 +34,20 @@ export class RecipeService{
 
     getRecipe(index:number): Recipe{
         return this.recipes[index];
+    }
+
+    addRecipe(newRecipe: Recipe){
+        this.recipes.push(newRecipe);
+        this.recipesChanged.next(this.recipes.slice())
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice())
+    }
+
+    deleteRecipe(index: number){
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
