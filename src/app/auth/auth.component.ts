1eb1e88fb1046b0ba8b7652bfc728191ead27b58
pavananumoju <pavananumoju@gmail.com>
2021-09-19 15:49:56 +0530

    signup, loading spinner, error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,45 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent{}
+export class AuthComponent{
+    isLoginMode = true;
+    isLoading = false;
+    error:string = null;
+
+    constructor(private authService: AuthService){}
+
+    onSwitchMode(){
+        this.isLoginMode = !this.isLoginMode;
+    }
+
+    onSubmit(authForm: NgForm){
+        // console.log(authForm.value);
+        if(!authForm.valid){
+            return;
+        }
+
+        if(this.isLoginMode){
+            // ...
+        } else{
+            const email = authForm.value.email;
+            const password = authForm.value.password;
+            this.isLoading = true;
+            this.authService.signUp(email, password)
+            .subscribe((responseData) => {
+                this.isLoading = false;
+                console.log(responseData);
+            }, error => {
+                this.isLoading = false;
+                this.error = error.message;
+                // console.log(error);
+            });
+        }
+
+        authForm.reset();
+    }
+}
